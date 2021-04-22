a29aa6bcdb7f4f8e8a0289b4ce9f7e138e33df0c
Ben Bastow <1839819@brunel.ac.uk>
2021-04-22 19:21:47 +0100

    Initial Commit

---
+++ Ultimate Portfolio/Ultimate_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_PortfolioApp.swift
+//  Ultimate Portfolio
+//
+//  Created by Ben Bastow on 22/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
